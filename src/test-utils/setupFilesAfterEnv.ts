3b6c2c43fe3daaf12bd87fd84fdaa937e6c58d63
Carlo Schneider <contact@carloschneider.dev>
2023-01-25 12:19:28 -0300

    fix: change matchMedia mock

--- src/test-utils/setupFilesAfterEnv.ts
+++ src/test-utils/setupFilesAfterEnv.ts
@@ -8,16 +8,11 @@ Schema.warning = function () {
   return void 0
 }
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-  }))
-})
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }
+  }
