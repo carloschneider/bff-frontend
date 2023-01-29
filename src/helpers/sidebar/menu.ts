a57dc9f38a7de96cab6e5fa639eb5ba5e63f273b
Carlo Schneider <contact@carloschneider.dev>
2023-01-29 15:47:14 -0300

    feat: move menu helpers to another directory

---
+++ src/helpers/sidebar/menu.ts
@@ -0,0 +1,36 @@
+import type { Location, Params } from '@remix-run/router'
+
+export type MatchesType = {
+  id: string
+  pathname: string
+  params: Params<string>
+  data: unknown
+  handle: unknown
+}
+
+export const selectedKey = (
+  matches: MatchesType[],
+  location: Location
+): string => {
+  const lastMatch = matches.at(-1)
+
+  if (!lastMatch) {
+    return location.pathname
+  }
+
+  const { pathname, params } = lastMatch
+
+  if (!Object.keys(params).length) {
+    return location.pathname
+  }
+
+  let path = pathname
+
+  Object.keys(params).forEach((item: string) => {
+    const param = params[item] as string
+
+    path = path.replace(`/${param}`, '')
+  })
+
+  return path
+}
